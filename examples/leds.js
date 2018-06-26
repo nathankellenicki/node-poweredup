b4cd5fd40b09693b6a8ed457d9471dccf0c84577
Nathan Kunicki <me@nathankunicki.com>
2018-06-26 23:21:37 +0100

    LED example across multiple hubs

---
+++ examples/leds.js
@@ -0,0 +1,27 @@
+const LPF2 = require("..").LPF2;
+
+const lpf2 = new LPF2();
+lpf2.scan(); // Start scanning for hubs
+
+console.log("Looking for Hubs...");
+
+lpf2.on("discover", async (hub) => { // Wait to discover hubs
+
+    await hub.connect(); // Connect to hub
+    console.log("Connected to Hub!");
+
+});
+
+let color = 0;
+setInterval(() => {
+
+    const hubs = lpf2.getConnectedDevices();
+    hubs.forEach((hub) => {
+        hub.setLEDColor(color);
+    })
+    color++;
+    if (color > 10) {
+        color = 0;
+    }
+
+}, 2000);
