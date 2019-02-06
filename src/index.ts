e7b5e977d98e541fed9ef9ee7824e7f3c2c8c98a
Nathan Kellenicki <nathan.kellenicki@sony.com>
2019-02-06 14:21:20 -0800

    Conditional export of poweredup class

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,17 @@ import { BoostMoveHub } from "./boostmovehub";
 import * as Consts from "./consts";
 import { DuploTrainBase } from "./duplotrainbase";
 import { Hub } from "./hub";
-import { PoweredUP } from "./poweredup";
+import { PoweredUP as PoweredUPNode } from "./poweredup-node";
 import { PUPHub } from "./puphub";
 import { PUPRemote } from "./pupremote";
 import { WeDo2SmartHub } from "./wedo2smarthub";
+import { isBrowserContext } from "./utils";
+
+let PoweredUP;
 
 export default PoweredUP;
 export { PoweredUP, Hub, WeDo2SmartHub, BoostMoveHub, PUPHub, PUPRemote, DuploTrainBase, Consts };
+
+if (!isBrowserContext) {
+    PoweredUP = PoweredUPNode;
+}
