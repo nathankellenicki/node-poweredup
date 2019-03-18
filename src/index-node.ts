c0f2bc9c0c4be889781f686efdc6af37e54f8ac5
Nathan Kellenicki <nathan.kellenicki@sony.com>
2019-03-18 16:10:34 -0700

    Node now has isWebBluetooth

--- src/index-node.ts
+++ src/index-node.ts
@@ -7,5 +7,7 @@ import { PUPHub } from "./puphub";
 import { PUPRemote } from "./pupremote";
 import { WeDo2SmartHub } from "./wedo2smarthub";
 
+import { isWebBluetooth } from "./utils";
+
 export default PoweredUP;
-export { PoweredUP, Hub, WeDo2SmartHub, BoostMoveHub, PUPHub, PUPRemote, DuploTrainBase, Consts };
+export { PoweredUP, Hub, WeDo2SmartHub, BoostMoveHub, PUPHub, PUPRemote, DuploTrainBase, Consts, isWebBluetooth };
