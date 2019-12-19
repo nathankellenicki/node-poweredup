9d05d64907d3e80844b29b495c6a7a59fe44c2d0
Leo Bonnargent <leo.bonnargent@fr.ey.com>
2019-12-19 09:04:50 +0100

    Color and distance configuration
    - back to namespace
    - change hub type names

--- src/devices/colordistancesensor.ts
+++ src/devices/colordistancesensor.ts
@@ -5,95 +5,6 @@ import { IDeviceInterface } from "../interfaces";
 import { DeviceType, HubType, IDeviceMode, ValueType } from "../consts";
 
 export class ColorDistanceSensor extends Device {
-
-    private static modes: { [name: string]: IDeviceMode } = {
-        COLOR: {
-            /**
-             * Emits when a color sensor is activated.
-             * @event ColorDistanceSensor#color
-             * @param {string} port
-             * @param {Color} color
-             */
-            input: true,
-            event: "color",
-            values: { type: ValueType.UInt8, count: 1, min: 0, max: 255 },
-            num: {
-                [HubType.BOOST_MOVE_HUB]: 0x00,
-                [HubType.CONTROL_PLUS_HUB]: 0x00,
-                [HubType.POWERED_UP_HUB]: 0x00,
-                [HubType.WEDO2_SMART_HUB]: 0x00
-            }
-        },
-        PROX: {
-            input: true,
-            event: "distance",
-            values: { type: ValueType.UInt8, count: 1, min: 0, max: 10 },
-            num: {
-                [HubType.BOOST_MOVE_HUB]: 0x01,
-                [HubType.CONTROL_PLUS_HUB]: 0x01,
-                [HubType.POWERED_UP_HUB]: 0x01
-            }
-        },
-        COUNT: {
-            input: true,
-            event: "count",
-            values: { type: ValueType.UInt8, count: 1, min: 0 },
-            num: {
-                [HubType.BOOST_MOVE_HUB]: 0x02,
-                [HubType.CONTROL_PLUS_HUB]: 0x02,
-                [HubType.POWERED_UP_HUB]: 0x02
-            }
-        },
-        REFLT: {
-            input: true,
-            event: "reflectivity",
-            values: { type: ValueType.UInt8, count: 1, min: 0, max: 100 },
-            num: {
-                [HubType.BOOST_MOVE_HUB]: 0x03,
-                [HubType.CONTROL_PLUS_HUB]: 0x03,
-                [HubType.POWERED_UP_HUB]: 0x03
-            }
-        },
-        AMBI: {
-            input: true,
-            event: "luminosity",
-            values: { type: ValueType.UInt8, count: 1, min: 0, max: 100 },
-            num: {
-                [HubType.BOOST_MOVE_HUB]: 0x04,
-                [HubType.CONTROL_PLUS_HUB]: 0x04,
-                [HubType.POWERED_UP_HUB]: 0x04
-            }
-        },
-        SOME_OUTPUT_MODE: {
-            input: false,
-            num: {}
-        },
-        RGB_I: {
-            input: true,
-            event: "rgb",
-            values: { type: ValueType.UInt8, count: 3, min: 0, max: 255 },
-            num: {
-                [HubType.BOOST_MOVE_HUB]: 0x06,
-                [HubType.CONTROL_PLUS_HUB]: 0x06,
-                [HubType.POWERED_UP_HUB]: 0x06
-            }
-        },
-        OTHER_OUTPUT_MODE: {
-            input: false,
-            num: {}
-        },
-        SPEC_1: {
-            input: true,
-            event: "colorAndDistance",
-            values: { type: ValueType.UInt8, count: 4, min: 0, max: 255 },
-            num: {
-                [HubType.BOOST_MOVE_HUB]: 0x08,
-                [HubType.CONTROL_PLUS_HUB]: 0x08,
-                [HubType.POWERED_UP_HUB]: 0x08
-            }
-        }
-    };
-
     constructor (hub: IDeviceInterface, portId: number) {
         super(hub, portId, ColorDistanceSensor.modes, DeviceType.COLOR_DISTANCE_SENSOR);
     }
@@ -109,4 +20,94 @@ export class ColorDistanceSensor extends Device {
 
         return data;
     }
+}
+
+export namespace ColorDistanceSensor {
+    export const modes: { [name: string]: IDeviceMode } = {
+        COLOR: {
+            /**
+             * Emits when a color sensor is activated.
+             * @event ColorDistanceSensor#color
+             * @param {string} port
+             * @param {Color} color
+             */
+            input: true,
+            event: "color",
+            values: { type: ValueType.UInt8, count: 1, min: 0, max: 255 },
+            num: {
+                [HubType.MOVE_HUB]: 0x00,
+                [HubType.TECHNIC_MEDIUM_HUB]: 0x00,
+                [HubType.HUB]: 0x00,
+                [HubType.WEDO2_SMART_HUB]: 0x00
+            }
+        },
+        PROX: {
+            input: true,
+            event: "distance",
+            values: { type: ValueType.UInt8, count: 1, min: 0, max: 10 },
+            num: {
+                [HubType.MOVE_HUB]: 0x01,
+                [HubType.TECHNIC_MEDIUM_HUB]: 0x01,
+                [HubType.HUB]: 0x01
+            }
+        },
+        COUNT: {
+            input: true,
+            event: "count",
+            values: { type: ValueType.UInt8, count: 1, min: 0 },
+            num: {
+                [HubType.MOVE_HUB]: 0x02,
+                [HubType.TECHNIC_MEDIUM_HUB]: 0x02,
+                [HubType.HUB]: 0x02
+            }
+        },
+        REFLT: {
+            input: true,
+            event: "reflectivity",
+            values: { type: ValueType.UInt8, count: 1, min: 0, max: 100 },
+            num: {
+                [HubType.MOVE_HUB]: 0x03,
+                [HubType.TECHNIC_MEDIUM_HUB]: 0x03,
+                [HubType.HUB]: 0x03
+            }
+        },
+        AMBI: {
+            input: true,
+            event: "luminosity",
+            values: { type: ValueType.UInt8, count: 1, min: 0, max: 100 },
+            num: {
+                [HubType.MOVE_HUB]: 0x04,
+                [HubType.TECHNIC_MEDIUM_HUB]: 0x04,
+                [HubType.HUB]: 0x04
+            }
+        },
+        SOME_OUTPUT_MODE: {
+            input: false,
+            num: {}
+        },
+        RGB_I: {
+            input: true,
+            event: "rgb",
+            values: { type: ValueType.UInt8, count: 3, min: 0, max: 255 },
+            num: {
+                [HubType.MOVE_HUB]: 0x06,
+                [HubType.TECHNIC_MEDIUM_HUB]: 0x06,
+                [HubType.HUB]: 0x06
+            }
+        },
+        OTHER_OUTPUT_MODE: {
+            input: false,
+            num: {}
+        },
+        SPEC_1: {
+            input: true,
+            event: "colorAndDistance",
+            values: { type: ValueType.UInt8, count: 4, min: 0, max: 255 },
+            num: {
+                [HubType.MOVE_HUB]: 0x08,
+                [HubType.TECHNIC_MEDIUM_HUB]: 0x08,
+                [HubType.HUB]: 0x08
+            }
+        }
+    };
 }
