bd3329d9d9bcc588583ce6400ee987e3ccd54981
Sergey Alyaev <saly@norceresearch.no>
2022-08-07 17:03:26 +0200

    Added an example with led control of Duplo Train

--- examples/duplo_test.js
+++ examples/duplo_test.js
@@ -6,13 +6,24 @@ poweredUP.on("discover", async (hub) => { // Wait to discover a Hub
     await hub.connect(); // Connect to the Hub
     console.log("Connected");
     let devices = hub.getDevices()
-    console.log(`Devices ${devices}`);
     for (let i = 0; i<devices.length; ++i){
         let device = devices[i];
         console.log(`Device port ${device.portName} type ${device.type}`);
     }
+    const hubLed = PoweredUP.Consts.DeviceTypeNames.HUB_LED;
+    let lights = hub.getDevicesByType(hubLed);
+    for (let i = 0; i<lights.length; ++i){
+        let device = lights[i];
+        console.log(`HubLed port ${device.portName} type ${device.type}`);
+    }
 
     const motor = await hub.waitForDeviceAtPort("MOTOR");
+    const light = await hub.waitForDeviceByType(hubLed);
+    console.log("Setting running light to RED for 2 seconds");
+    light.setColor(PoweredUP.Consts.ColorNames.RED);
+    await hub.sleep(2000);
+    console.log("Setting running light to WHITE");    
+    light.setColor(PoweredUP.Consts.ColorNames.WHITE);
 
     while (true) { // Repeat indefinitely
         console.log("Running motor at speed 100 for 2 seconds");
